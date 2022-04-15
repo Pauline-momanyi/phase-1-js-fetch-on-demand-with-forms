a62cd25040f8b188745257a563918505343ed424
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-15 18:29:07 +0300

    Fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', event =>{
+      event.preventDefault();
+      console.log(event);
+      const input = event.target.children[1].value
+      console.log(input);
+
+      fetch(`http://localhost:3000/movies/${input}`)
+      .then(resp=>resp.json())
+      .then(json=>{
+          console.log(json);
+          const title = document.querySelector('section#movieDetails h4')
+          //const title2 = document.querySelector('h4')
+          const summary = document.querySelector('section#movieDetails p')
+          //console.log(summary);
+          title.innerText = json.title
+          summary.textContent = json.summary
+          
+      })
+  }
+  )
 }
 
 document.addEventListener('DOMContentLoaded', init);
